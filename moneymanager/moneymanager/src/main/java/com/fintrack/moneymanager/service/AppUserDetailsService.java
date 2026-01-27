87ac395fcd5c254426c750f5e0019a2e343425c4
choksh-aware <choksh2003@gmail.com>
2026-01-27 08:16:46 +0530

    done changes with service

--- moneymanager/moneymanager/src/main/java/com/fintrack/moneymanager/service/AppUserDetailsService.java
+++ moneymanager/moneymanager/src/main/java/com/fintrack/moneymanager/service/AppUserDetailsService.java
@@ -51,3 +51,4 @@ public class AppUserDetailsService implements UserDetailsService {
                 .build();
     }
 }
+
--- moneymanager/moneymanager/src/main/java/com/fintrack/moneymanager/service/IncomeService.java
+++ moneymanager/moneymanager/src/main/java/com/fintrack/moneymanager/service/IncomeService.java
@@ -180,3 +180,8 @@ public class IncomeService {
         }
     }
 }
+
+
+
+
+
